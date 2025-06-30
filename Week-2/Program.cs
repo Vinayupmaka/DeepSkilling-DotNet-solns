406314684e804497b7535cada65e2774a6a27d8a
Vinay Upmaka <117618487+Vinayupmaka@users.noreply.github.com>
2025-06-30 17:16:53 +0530

    Add files via upload

--- Week-2/Program.cs
+++ Week-2/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CustomerCommLib;
+using Moq;
+using NUnit.Framework;
 
-namespace CalcLibrary.Tests
+namespace Tests
 {
-    internal class Program
+    [TestFixture]
+    public class CustomerCommTests
     {
-        static void Main(string[] args)
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerComm _customerComm;
+
+        [OneTimeSetUp]
+        public void Setup()
         {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender
+                .Setup(sender => sender.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            CustomerCommLib.CustomerComm _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            var result = _customerComm.SendMailToCustomer();
+            Assert.That(result, Is.True);
         }
     }
 }
