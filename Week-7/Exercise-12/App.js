4a402a9b08ddf265fdc3f97fb297cd822b762627
Vinay Upmaka <117618487+Vinayupmaka@users.noreply.github.com>
2025-08-03 22:02:08 +0530

    Create App.js

---
+++ Week-7/Exercise-12/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginControl from './Components/LoginControl';
+import './App.css';
+function App() {
+return (
+<div className="App">
+<h1>✈️ Ticket Booking App</h1>
+<LoginControl />
+</div>
+);
+}
+export default App;
