a6921c03563937b88e28e499f37bf573c0c7700b
Vinay Upmaka <117618487+Vinayupmaka@users.noreply.github.com>
2025-08-03 22:06:34 +0530

    Create App.js

---
+++ Week-7/Exercise-13/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DisplayControl from './Components/DisplayControl';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayControl />
+    </div>
+  );
+}
+
+export default App;
